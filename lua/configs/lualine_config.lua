c359028b0169097102468e19bbf42386e407d014
Beloin Sena <beloin.rodrigues@gmail.com>
2024-04-03 20:04:43 -0300

    Removed dap.close on listener

--- lua/configs/lualine_config.lua
+++ lua/configs/lualine_config.lua
@@ -3,20 +3,18 @@ local M = {}
 M.config = function()
     local lualine = require('lualine')
     lualine.setup({
-      sections = {
-        lualine_z = {
-            {
-                function()
-                    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-                        if vim.api.nvim_buf_get_option(buf, 'modified') then
+        sections = {
+            lualine_z = {{function()
+                for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+                    if vim.api.nvim_buf_get_option(buf, 'modified') then
+                        if not vim.bo[buf].readonly then
                             return 'Unsaved buffers' -- any message or icon
                         end
                     end
-                    return ''
-                end,
-            },
-        },
-    },
+                end
+                return ''
+            end}}
+        }
     })
 end
 
--- lua/configs/masondap.lua
+++ lua/configs/masondap.lua
@@ -129,13 +129,13 @@ dap.listeners.before.launch.dapui_config = function()
     dapui.open()
 end
 
-dap.listeners.before.event_terminated.dapui_config = function()
-    dapui.close()
-end
+-- dap.listeners.before.event_terminated.dapui_config = function()
+--     dapui.close()
+-- end
 
-dap.listeners.before.event_exited.dapui_config = function()
-    dapui.close()
-end
+-- dap.listeners.before.event_exited.dapui_config = function()
+--     dapui.close()
+-- end
 
 vim.keymap.set({'i', 'n'}, "<F5>", function()
     -- (Re-)reads launch.json if present -- Use rl.reload();
