a8afa71d92d0c127d5b2a48f6529f340b39a4885
Beloin Rodrigues <beloinrodrigues.lds@grupomulti.com.br>
2024-06-14 16:14:17 -0300

    Added rest.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -615,6 +615,23 @@ return {
   {
     'rhysd/conflict-marker.vim',
     event = 'VeryLazy'
+  },
+
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
+    }
+  },
+  {
+    "rest-nvim/rest.nvim",
+    ft = "http",
+    dependencies = { "luarocks.nvim" },
+    config = function()
+      require("rest-nvim").setup()
+    end,
   }
 
 }
